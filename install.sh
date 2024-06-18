290a9097f92f644e5ee00af9904444e2a20fcb9c
Issei <116876441+Issei-177013@users.noreply.github.com>
2024-06-18 12:39:44 +0330

    fixed bug install.sh
    Fix directory detection bug
    Run commands with root user

--- install.sh
+++ install.sh
@@ -1,53 +1,55 @@
 #!/bin/bash
 
-
-
+# Function to delete and replace cron job
 delete_and_replace_cron_job() {
   local cron_job_name="@reboot /bin/bash $script_dir/install.sh"
-  crontab -l | grep -v -F "$cron_job_name" | crontab -
-  (crontab -l ; echo "$cron_job_name") | crontab -
+  sudo crontab -l | grep -v -F "$cron_job_name" | sudo crontab -
+  (sudo crontab -l ; echo "$cron_job_name") | sudo crontab -
 }
 
+# Function to add a three-minute cron job
 add_three_minute_cron_job() {
   local cron_job_name="*/3 * * * * /bin/bash $script_dir/delete_and_replace_cron_job.sh"
-  crontab -l | grep -v -F "$cron_job_name" | crontab -
-  (crontab -l ; echo "$cron_job_name") | crontab -
+  sudo crontab -l | grep -v -F "$cron_job_name" | sudo crontab -
+  (sudo crontab -l ; echo "$cron_job_name") | sudo crontab -
 }
 
+# Function to setup Apache and files
 setup_apache_and_files() {
   echo "Setting up Apache2 and files..."
   
   # Install and configure Apache2
-  apt update
-  apt install -y apache2
+  sudo apt update
+  sudo apt install -y apache2
 
   # Navigate to HTML directory
-  cd /var/www/html
+  cd /var/www/html || exit
 
   # Create files
-  dd if=/dev/zero of=COD.part01.rar bs=100M count=1
-  dd if=/dev/zero of=COD.part02.rar bs=100M count=1
-  dd if=/dev/zero of=COD.part03.rar bs=100M count=1
-  dd if=/dev/zero of=COD.part04.rar bs=100M count=1
-  dd if=/dev/zero of=COD.part05.rar bs=100M count=1
-  dd if=/dev/zero of=COD.part06.rar bs=100M count=1
-  dd if=/dev/zero of=COD.part07.rar bs=100M count=1
-  dd if=/dev/zero of=COD.part08.rar bs=100M count=1
+  sudo dd if=/dev/zero of=COD.part01.rar bs=100M count=1
+  sudo dd if=/dev/zero of=COD.part02.rar bs=100M count=1
+  sudo dd if=/dev/zero of=COD.part03.rar bs=100M count=1
+  sudo dd if=/dev/zero of=COD.part04.rar bs=100M count=1
+  sudo dd if=/dev/zero of=COD.part05.rar bs=100M count=1
+  sudo dd if=/dev/zero of=COD.part06.rar bs=100M count=1
+  sudo dd if=/dev/zero of=COD.part07.rar bs=100M count=1
+  sudo dd if=/dev/zero of=COD.part08.rar bs=100M count=1
 
   # Set file permissions
-  chmod 644 /var/www/html/COD.part01.rar
-  chmod 644 /var/www/html/COD.part02.rar
-  chmod 644 /var/www/html/COD.part03.rar
-  chmod 644 /var/www/html/COD.part04.rar
-  chmod 644 /var/www/html/COD.part05.rar
-  chmod 644 /var/www/html/COD.part06.rar
-  chmod 644 /var/www/html/COD.part07.rar
-  chmod 644 /var/www/html/COD.part08.rar
+  sudo chmod 644 COD.part01.rar
+  sudo chmod 644 COD.part02.rar
+  sudo chmod 644 COD.part03.rar
+  sudo chmod 644 COD.part04.rar
+  sudo chmod 644 COD.part05.rar
+  sudo chmod 644 COD.part06.rar
+  sudo chmod 644 COD.part07.rar
+  sudo chmod 644 COD.part08.rar
 }
 
+# Determine script directory
 script_dir="$(dirname "$(readlink -f "$BASH_SOURCE")")"
 
-# Ensure the script is being run with sudo
+# Ensure script is run as root
 if [[ $EUID -ne 0 ]]; then
    echo "This script must be run as root" 
    exit 1
@@ -60,6 +62,7 @@ echo "2. Kharej"
 
 read -p "Enter your choice (1 or 2): " choice
 
+# Case statement based on user input
 case $choice in
   1)
     setup_apache_and_files
@@ -74,13 +77,12 @@ case $choice in
 esac
 
 # Update package list and install necessary packages
-apt update
-apt install -y python3 python3-pip
+sudo apt update
+sudo apt install -y python3 python3-pip
 
 # Install Python dependencies
-pip3 install requests==2.26.0
-pip3 install tqdm==4.66.4
-
+sudo pip3 install requests==2.26.0
+sudo pip3 install tqdm==4.66.4
 
 # Check if url.txt exists, create it if not
 url_file="$script_dir/url.txt"
@@ -95,13 +97,11 @@ if [ ! -f "$url_file" ]; then
     echo "http://$ip_address/COD.part06.rar" >> "$url_file"
     echo "http://$ip_address/COD.part07.rar" >> "$url_file"
     echo "http://$ip_address/COD.part08.rar" >> "$url_file"
-    # Repeat for other parts if needed
     echo "File urls added to $url_file"
 fi
 
-
 # Run Python script
-python3 "$script_dir/fakeup.py"
+python3 "$script_dir/fake-upload/fakeup.py"
 
 # Set up cron jobs
 delete_and_replace_cron_job
